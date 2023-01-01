0d70db3fe5c5b653f415778a9a18ebb2e1704ccb
rebecanonato89 <rebecanonato89@gmail.com>
2022-12-31 22:08:19 -0300

    equinox

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,8 @@
-import React from 'react';
-import logo from './logo.svg';
 import './App.css';
+import face from './images/facebook.png';
+import insta from './images/instagram.png';
+import logo from './images/logo.png';
+import whats from './images/whatsapp.png';
 
 function App() {
   return (
@@ -8,16 +10,35 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Edit <code>src/App.tsx</code> and save to reload.
+          Estamos em construção. <br />{' '}
+          <strong>Enquanto isso nos acompanhe nas redes sociais.</strong>
         </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        <div className="social">
+          <a
+            className="App-link"
+            href="https://www.instagram.com/equinox.inovacoes/"
+            target="_blank"
+            rel="noopener noreferrer"
+          >
+            <img src={insta} className="face" alt="face" />
+          </a>
+          <a
+            className="App-link"
+            href="https://wa.me/5535988682079"
+            target="_blank"
+            rel="noopener noreferrer"
+          >
+            <img src={whats} className="whats" alt="whats" />
+          </a>
+          <a
+            className="App-link"
+            href="https://www.facebook.com/equinoxsolucoes"
+            target="_blank"
+            rel="noopener noreferrer"
+          >
+            <img src={face} className="face" alt="face" />
+          </a>
+        </div>
       </header>
     </div>
   );
